5ddf388e107401133dc8377791b419a0a9339dfb
wasteland_rider <wasteland.rider.7bf@gmail.com>
2019-07-22 19:32:13 +0300

    Add timeout and assertion check.

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,12 @@
-from .basepage import BasePage
+from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
-	"""docstring for MainPage"""
+	"""MainPage. Class describing main page to test."""
 	
 	def go_to_login_page(self):
-    link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    link.click()
+		link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+		link.click()
+
+	def should_be_login_link(self):
+		assert self.is_element_present(By.CSS_SELECTOR, "#login_link_1"), 'Login link is not presented.'
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
-from .pages.main_page import MainPage
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    #page.go_to_login_page()
+    page.should_be_login_link()
