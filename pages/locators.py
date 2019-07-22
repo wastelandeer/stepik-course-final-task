fd434b53d4427fda3fca886ff27590aa3c061a08
wasteland_rider <wasteland.rider.7bf@gmail.com>
2019-07-22 19:46:22 +0300

    Add login link locator.

---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators(object):
+	"""docstring for MainPageLocators"""
+	LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')	
--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,8 @@
 from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    #link = "http://selenium1py.pythonanywhere.com/"
+    link ='http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     
     page = MainPage(browser, link)
     page.open()
