2130b4ea177310a3828a1551d7904040a67739c5
wasteland_rider <wasteland.rider.7bf@gmail.com>
2019-07-23 09:04:45 +0300

    Add tests checking if login/reg form exists.

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,9 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
 	"""docstring for MainPageLocators"""
-	LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')	
+	LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+class LoginPageLocators(object):
+	"""docstring for LoginPageLocators"""
+	LOGIN_FORM = (By.CSS_SELECTOR, 'form#login_form')
+	REGISTER_FORM = (By.CSS_SELECTOR, 'form#register_form')
--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,18 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     #link = "http://selenium1py.pythonanywhere.com/"
-    link ='http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+    #link ='http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+    link = 'http://selenium1py.pythonanywhere.com/'
     
-    page = MainPage(browser, link)
-    page.open()
+    #page = MainPage(browser, link)
+    #page.open()
     #page.go_to_login_page()
-    page.should_be_login_link()
+    #page.should_be_login_link()
+
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    #login_page.should_be_login_url()
+    #login_page.should_be_login_form()
+    #login_page.should_be_register_form()
